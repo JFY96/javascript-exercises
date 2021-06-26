88de2be2b7e3aa9606e2accb34ac76fca18d9a81
JFY96 <36199863+JFY96@users.noreply.github.com>
2021-06-27 00:39:42 +0100

    Complete solutions to helloWorld, repeatString, reverseString, removeFromArray, sumAll, leapYears, tempConversion

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,5 +1,8 @@
-const repeatString = function() {
-
+const repeatString = function(string, times) {
+    if (times < 0) return "ERROR";
+    let returnStr = "";
+    for (let i = 0; i < times; i++) returnStr += string;
+    return returnStr;
 };
 
 module.exports = repeatString;
--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,5 +1,11 @@
-const sumAll = function() {
-
+const sumAll = function(from, to) {
+    if (!Number.isInteger(from) || !Number.isInteger(to) || from < 0 || to < 0) return "ERROR";
+    const temp = from;
+    if (to < from) {
+        from = to;
+        to = temp;
+    }
+    return ((to*(to+1))/2) - ((from*(from-1))/2);
 };
 
 module.exports = sumAll;
